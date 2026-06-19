775d348ec34ba676dba6cb983cfd4ad16fa04621
Muhammad Jawad Saeed <jawad.saeed@buggcy.com>
2026-06-19 13:17:38 +0500

    update url

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -3,7 +3,7 @@ import { useAuthStore } from "../store/auth.store";
 
 console.log(import.meta.env.VITE_API_URL);
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:4000/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use((config) => {
