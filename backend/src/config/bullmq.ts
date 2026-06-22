2865a46e25d7fbbc4714e13c34dd5dd3af11a699
Muhammad Jawad Saeed <jawad.saeed@buggcy.com>
2026-06-22 13:35:21 +0500

    REDIS UPDATE

--- backend/src/config/bullmq.ts
+++ backend/src/config/bullmq.ts
@@ -1,6 +1,11 @@
 import { env } from "./env.js";
 
+const redisUrl = new URL(env.REDIS_URL);
+
 export const bullmqConnection = {
-  url: env.REDIS_URL,
+  host: redisUrl.hostname,
+  port: Number(redisUrl.port),
+  username: redisUrl.username,
+  password: redisUrl.password,
   maxRetriesPerRequest: null,
 };
